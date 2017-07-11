ab07028d52e1ffc3d276882c9ed811b4e0180265
marcr <mr.ribault@gmail.com>
2017-07-11 15:48:10 +0200

    first commit

---
+++ ex2.rb
@@ -0,0 +1,56 @@
+
+def grr(x,y)
+
+lettres =[]
+lettres = x.split("") 
+
+print lettres
+
+lettresHSC=[]
+lettres.each { |string|  lettresHSC << string.ord 
+}
+
+puts lettresHSC
+
+lettresHSC.each {|x|
+  x += y
+ 
+  print "#{x.chr}" 
+
+}
+
+end
+
+grr("ab!bc", 1)	
+
+puts " What a string! Bmfy f xywnsl!"
+
+=begin 
+mots = x.split("!")
+print mots
+
+["ryan", "jane"].each {|string| puts "#{string[0].upcase}#{string[1..-1]}"}
+
+def cdc(x,z)
+
+
+	y=0
+	y = x.ord + z
+	puts y.chr
+
+end
+
+cdc("b",4) =end
+
+# a.each { |a| puts "v" }
+
+
+#cdc("a",2)
+#for
+
+String[]
+tab=[1,4,0]
+=end
+
+
+
